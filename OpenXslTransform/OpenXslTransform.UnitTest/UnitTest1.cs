d37e4f9299c837ada4ce51bb6ac67629843feaca
alxhu-dev <32041622+alxhu-dev@users.noreply.github.com>
2024-03-31 22:38:28 +0200

    Add unit test solution

---
+++ OpenXslTransform/OpenXslTransform.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OpenXslTransform.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
